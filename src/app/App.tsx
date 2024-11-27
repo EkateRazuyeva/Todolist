cc0fe2f6aeabe586731d671bb834b708732e06e9
Ekate Razuyeva <25ky-ky08@mail.ru>
2024-11-27 18:00:45 +0300

    16 - react router dom, react hook form, auth flow

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,19 +1,38 @@
 import CssBaseline from "@mui/material/CssBaseline"
 import { ThemeProvider } from "@mui/material/styles"
 import { Header, ErrorSnackbar } from "common/components"
-import { useAppSelector } from "common/hooks"
+import { useAppDispatch, useAppSelector } from "common/hooks"
 import { getTheme } from "common/theme"
 import { selectThemeMode } from "./appSelectors"
-import { Main } from "./Main"
-
+import { Outlet } from "react-router-dom"
+import { useEffect } from "react"
+import { initializeAppTC } from "../features/auth/model/auth-reducer"
+import { selectIsInitialized } from "../features/auth/model/authSelectors"
+import { CircularProgress } from "@mui/material"
+import s from "./App.module.css"
 export const App = () => {
   const themeMode = useAppSelector(selectThemeMode)
+  const isInitialized = useAppSelector(selectIsInitialized)
+  const dispatch = useAppDispatch()
+
+  useEffect(() => {
+    dispatch(initializeAppTC())
+  })
 
   return (
     <ThemeProvider theme={getTheme(themeMode)}>
       <CssBaseline />
-      <Header />
-      <Main />
+      {isInitialized && (
+        <>
+          <Header />
+          <Outlet />
+        </>
+      )}
+      {!isInitialized && (
+        <div className={s.circularProgressContainer}>
+          <CircularProgress size={150} thickness={3} />
+        </div>
+      )}
       <ErrorSnackbar />
     </ThemeProvider>
   )
