6630a3e39e5c78bb3d1d6386fcb145e8054daba4
Ekate Razuyeva <25ky-ky08@mail.ru>
2024-12-05 22:41:26 +0300

    18 -todolists API on RTK query

--- src/app/store.ts
+++ src/app/store.ts
@@ -3,6 +3,8 @@ import { authReducer, authSlice } from "../features/auth/model/authSlice"
 import { tasksReducer, tasksSlice } from "../features/todolists/model/tasksSlice"
 import { todolistsReducer, todolistsSlice } from "../features/todolists/model/todolistsSlice"
 import { appReducer, appSlice } from "./appSlice"
+import { setupListeners } from "@reduxjs/toolkit/query"
+import { baseApi } from "./baseApi"
 
 export const store = configureStore({
   reducer: {
@@ -10,8 +12,12 @@ export const store = configureStore({
     [todolistsSlice.name]: todolistsReducer,
     [appSlice.name]: appReducer,
     [authSlice.name]: authReducer,
+    [baseApi.reducerPath]: baseApi.reducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(baseApi.middleware),
 })
+
+setupListeners(store.dispatch)
 
 export type RootState = ReturnType<typeof store.getState>
 
